b11dbfa0423fdda305c38590db3ca08807cf72ee
Myles Lewando <myles.lewando@opendataservices.coop>
2025-02-25 10:23:52 +0000

    Add comments throughout JavaScript
    Also add keyboard accessibility to download buttons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-import { endpoint } from "./endpoint.js";
+import { endpoint } from './endpoint.js';
 
 let list;
 
+// Function to fetch data from URL
 const getData = async (url) => {
   try {
     const response = await fetch(url);
@@ -15,7 +16,9 @@ const getData = async (url) => {
   }
 };
 
+// Function to generate HTML list of 'cards' from data
 const populateList = (data) => {
+  // Restructure download formats data into a simple object
   const downloads = data.downloads.reduce((acc, current) => {
     acc[current.format] = current.url;
     return acc;
@@ -58,20 +61,32 @@ const populateList = (data) => {
   list.appendChild(listItem);
 };
 
+// Function to toggle the downloads dropdown
 const toggleDownloads = (e) => {
   const dropdown = e.target.nextElementSibling;
   console.log(dropdown)
   dropdown.classList.toggle('show');
 }
 
+// Listen for the page to finish loading
 document.addEventListener('DOMContentLoaded', async () => {
   list = document.querySelector('#publisher-list');
+  
+  // Get data from the endpoint
   await getData(endpoint)
 
   const downloadButtons = document.querySelectorAll('.download-btn');
   downloadButtons.forEach((button) => {
+    // Add an event listener to all download buttons to toggle the downloads dropdown when clicked
     button.addEventListener('click', (e) => {
       toggleDownloads(e);
+    })
+    
+    // Add an event listener to all download buttons to toggle the downloads dropdown on pressing the enter key
+    button.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        toggleDownloads(e);
+      }
     })
   });
 });
